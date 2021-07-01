a5d13be8dbe0d524a91490ec85e82a328db87e77
nNlson rRmos Lopez <86324251+NelsonRamosL@users.noreply.github.com>
2021-07-01 17:33:54 -0400

    face prueba se incorporaron todos los requerimientos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,16 +5,29 @@ $(function () {
 
     $('[data-toggle="tooltip"]').tooltip();
     console.log("en el toggle");
-  
-
-
-  
 
 
 
 
 
+    $("#enviarCorreo").click(function () {
+        alert("El correo fue enviado correctamente...");
+        console.log("Correo Enviado");
+    });
 
-  
 
+    $("#titulo, #titulo2").dblclick(function () {
+        
+        $(this).css('color', 'red');
+        console.log("titulo en rojo");
+    });
+
+
+
+$(".targetatarget").click(function () {
+    $(this).parent().parent().toggle();
+        console.log("toggle targeta seleccionada");
+    });
+
+    
 })
